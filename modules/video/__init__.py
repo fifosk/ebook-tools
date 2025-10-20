a3342c46218f6e60e7550ec6190c662ec591e543
fifosk <95375263+fifosk@users.noreply.github.com>
2025-10-20 13:57:55 +0200

    Refactor sentence slide video helpers

---
+++ modules/video/__init__.py
@@ -0,0 +1,13 @@
+"""Video composition utilities."""
+
+from .slides import (
+    build_sentence_video,
+    generate_sentence_slide_image,
+    get_default_font_path,
+)
+
+__all__ = [
+    "build_sentence_video",
+    "generate_sentence_slide_image",
+    "get_default_font_path",
+]
